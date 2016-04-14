8349f460a4472ff50ff0968a17f1883e9f1478fb
Connor Leech <connorleech@gmail.com>
2016-04-14 00:15:30 -0700

    rrreeeeepurpose for my own nefarious ends

--- README.md
+++ README.md
@@ -1,34 +1,15 @@
 AngularJS GitHub Contributors
 =============================
 
+Todo list:
+- need to authenticate github requests (to avoid rate limit request errors)
+- show user's names, locations and emails
+- switch to ui-router to avoid dumb template caching problems
+- rewrite the whole thing in better angular
+- rewrite the whole thing in react/redux 
+
 This project is a port of the YUI 3.5 app
 [GitHub Contributors] to [AngularJS].
 
-The CSS has been taken unchanged from the original app. The html
-structure has also been taken from the original app, but it has been
-modified to work with [AngularJS].
-
-Known differenses between this port and the original app:
-
-* No pluralization of the word _contributor_ in the on hover title, of
-  the contributors thumbs.
-* One more json query to GitHub, 4 instead of 3.
-* Fewer lines of javascript code, 93 compared to 387, excluding lines
-  with comments and empty lines!
-
-Demo
-----
-View the app [AngularJS GitHub Contributors].
-
-Licenses
---------
-* All my code is licensed under the [Modified BSD License], excluding
-  the lib directories.
-* The html code in the partials directory is also licensed under the same
-  [BSD License] as the original app.
-
 [GitHub Contributors]: http://yuilibrary.com/yui/docs/app/app-contributors.html
-[AngularJS]: http://angularjs.org/
 [AngularJS GitHub Contributors]: http://daha.github.com/angularJS-github-contributors
-[Modified BSD License]: https://github.com/daha/angularJS-github-contributors/blob/master/LICENSE
-[BSD License]: http://yuilibrary.com/license/
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,36 +1,4 @@
-// Copyright (c) 2012, David Haglund
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-//     * Redistributions of source code must retain the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer.
-//
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials
-//       provided with the distribution.
-//
-//     * Neither the name of the copyright holder nor the names of its
-//       contributors may be used to endorse or promote products
-//       derived from this software without specific prior written
-//       permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
+
 /*globals angular */
 
 var watchForms = {
@@ -58,7 +26,6 @@ function RepoListCtrl($scope, $routeParams, githubResource) {
 
     $scope.repos = githubResource.get({user: $routeParams.user});
     $scope.user = $routeParams.user;
-
     $scope.watchForms = watchForms;
     $scope.forkForms = forkForms;
 }
@@ -94,14 +61,41 @@ function RepoCtrl($scope, $routeParams, githubResource) {
 }
 RepoCtrl.$inject = ['$scope', '$routeParams', 'githubResource'];
 
-function ContribListCtrl($scope, $routeParams, githubResource) {
-    'use strict';
+function ContribListCtrl($scope, $routeParams, $http, $timeout, githubResource) {
+    $scope.realContribs = [];
+    $scope.error = false;
 
     $scope.contributors = githubResource.get({
         'query': 'repos',
         'user': $routeParams.user,
         'repo': $routeParams.repo,
         'spec': 'contributors'
+    }, function(response){
+        var contributors = response.data;
+        
+        if(typeof contributors === 'object'){
+            $scope.error = contributors.message;
+            $timeout(function(){
+                $scope.error = $scope.error;
+                console.log($scope.error)
+            });
+            return;
+        }
+        contributors.forEach(function(c, i){
+            // request users profile (has their email address)
+            $http.jsonp('https://api.github.com/users/'+ c.login + '?callback=JSON_CALLBACK').then(function(res){
+                var user = res.data.data;
+                $scope.realContribs.push(user);
+                $timeout(function(){
+                    console.log($scope.realContribs);
+                    $scope.realContribs = $scope.realContribs;
+                });
+            });
+            
+        });
     });
+
+    
+
 }
-ContribListCtrl.$inject = ['$scope', '$routeParams', 'githubResource'];
+ContribListCtrl.$inject = ['$scope', '$routeParams', '$http', '$timeout', 'githubResource'];
